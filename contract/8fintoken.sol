19a3f4e1bcdd5088b08b057cc5677f8a91c821c8
amorev <shikto@mail.ru>
2022-04-26 11:56:15 +0300

    Testmode

--- contract/8fintoken.sol
+++ contract/8fintoken.sol
@@ -2,13 +2,18 @@
 pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract EightFinCon is ERC20 {
-    constructor () ERC20("8fin_test", "8fin_test") {
+    bool public testMode;
 
+    constructor (bool testModeVar) ERC20("8fin_test", "8fin_test") {
+            testMode = testModeVar;
+            _mint(tx.origin, 1000000000000000000000000000);
         }
     function mint (uint256 amount) public {
+        require(testMode, 'No testmode activated');
         _mint(tx.origin, amount);
     }
     function burn (uint256 amount) public {
+        require(testMode, 'No testmode activated');
         _burn(tx.origin, amount);
     }
 }
